a6ad4c61b2c070c5490cd86c7d37e8408346728e
kimyezzang97 <kimyezzang97@gmail.com>
2023-12-24 00:46:18 +0900

    기초수학 - 지수와 로그

---
+++ src/basicMath/LogStudy.java
@@ -0,0 +1,34 @@
+package basicMath;
+
+// 기초 수학 - 지수와 로그
+public class LogStudy {
+    public static void main(String[] args) {
+
+        // 1. 제곱, 제곱근, 지수
+        System.out.println("== 제곱 == ");
+        System.out.println(Math.pow(2, 3));
+        System.out.println(Math.pow(2, -3));
+        System.out.println(Math.pow(-2, -3));
+
+        System.out.println(Math.pow(2, 30));
+        System.out.printf("%.0f\n", Math.pow(2, 30));
+
+        System.out.println("== 제곱근 ==");
+        System.out.println(Math.sqrt(16));
+        System.out.println(Math.pow(16, 1.0/2)); // 위에랑 같음
+        System.out.println(Math.pow(16, 1.0/4));
+
+        // 참고) 절대 값
+        System.out.println("== 절대 값 ==");
+        System.out.println(Math.abs(5));
+        System.out.println(Math.abs(-5));
+
+        // 2. 로그
+        System.out.println("== 로그 ==");
+        System.out.println(Math.E); // 자연 상수
+        System.out.println(Math.log(Math.E));
+        System.out.println(Math.log10(1000)); // 10
+        System.out.println(Math.log(4) / Math.log(2));
+
+    }
+}
